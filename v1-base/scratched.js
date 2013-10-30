43b18450d2dfe9fc1f36e565b04959afcbf8c08b
Bill Merrill <evilai@gmail.com>
2013-10-30 11:29:16 -0700

    add the ability to edit a line

--- v1-base/scratched.js
+++ v1-base/scratched.js
@@ -1,19 +1,32 @@
 /*jslint browser: true*/
 
+/**
+    XXX: li.textContent will fail with ie8 and below
+ */
+
 var scratched = (function () {
     "use strict";
 
-    var input_page, input_box, the_list,
+    var input_page, input_box, the_list, current_item,
 
         /* model */
 
         insert_list_item = function (text) {
-            the_list.innerHTML += "<li>" + text + "</li>";
+            var new_li = document.createElement("li");
+            new_li.textContent = text;
+            the_list.appendChild(new_li);
+        },
+
+        update_list_item = function (text) {
+            current_item.textContent = text;
         },
 
         /* pages */
 
         show_input_page = function () {
+            if (current_item !== null) {
+                input_box.value = current_item.innerHTML;
+            }
             input_page.style.display = "block";
             input_box.focus();
         },
@@ -26,14 +39,25 @@ var scratched = (function () {
 
         cancel_input = function () {
             input_box.value = "";
+            current_item = null;
             hide_input_page();
         },
 
         save_item = function () {
-            insert_list_item(input_box.value);
+            if (current_item === null) {
+                insert_list_item(input_box.value);
+            } else {
+                update_list_item(input_box.value);
+            }
             input_box.value = "";
+            current_item = null;
             hide_input_page();
             return false;
+        },
+
+        select_item = function (e) {
+            current_item = e.target;
+            show_input_page();
         };
 
     return {
@@ -42,11 +66,14 @@ var scratched = (function () {
             input_page = document.getElementById("input_page");
             input_box = document.getElementById("new_item_text");
             the_list = document.getElementById("scratch_list");
+            current_item = null;
 
             // events
             document.getElementById("save_button").addEventListener("click", save_item);
             document.getElementById("plus").addEventListener("click", show_input_page);
             document.getElementById("cancel_button").addEventListener("click", cancel_input);
+            the_list.addEventListener("click", select_item);
+
         }
     };
 }());
